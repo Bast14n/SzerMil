de5e54dc7f449316136324a5f9bb1e53361399bb
bartek <gekarez@gmail.com>
2019-05-14 17:55:13 +0200

    Remove unused classes

--- app/src/main/java/com/example/szermil/restaurant_search/service/RestaurantService.java
+++
@@ -1,9 +0,0 @@
-package com.example.szermil.restaurant_search.service;
-
-import com.example.szermil.restaurant_search.model.Restaurant;
-
-import java.util.List;
-
-public interface RestaurantService {
-    List<Restaurant> getAllRestaurants();
-}
