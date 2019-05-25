0b3415ebd2315c582b47bdb1c5d94877f0a8b5fa
Krzysztof Pieprzyca <pieprzycakrzysztof@gmail.com>
2019-05-25 15:36:46 +0200

    add user screen

--- app/src/main/java/com/example/szermil/MainActivity.java
+++ app/src/main/java/com/example/szermil/MainActivity.java
@@ -8,6 +8,7 @@ import com.beardedhen.androidbootstrap.BootstrapLabel;
 import com.example.szermil.login.LoginActivity;
 import com.example.szermil.restaurant_search.RestaurantSearchActivity;
 import com.example.szermil.user.mark.UserMarksActivity;
+import com.example.szermil.user_profile.UserProfile;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,17 +23,18 @@ public class MainActivity extends AppCompatActivity {
         private void setButtonListeners() {
         BootstrapLabel loginButton = findViewById(R.id.loginButton);
         loginButton.setOnClickListener(v -> {
-            // TODO: implement login button
+            Intent intent = new Intent(getApplicationContext(), UserProfile.class);
+            startActivity(intent);
         });
 
         BootstrapLabel facebookLoginButton = findViewById(R.id.facebookLoginButton);
         facebookLoginButton.setOnClickListener(v -> {
-            Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+            Intent intent = new Intent(getApplicationContext(), RestaurantSearchActivity.class);
             startActivity(intent);
         });
         BootstrapLabel registerButton = findViewById(R.id.registerButton);
         registerButton.setOnClickListener(v -> {
-            Intent intent = new Intent(getApplicationContext(), RestaurantSearchActivity.class);
+            Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
             startActivity(intent);
         });
 
